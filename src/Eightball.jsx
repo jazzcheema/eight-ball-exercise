6ca1ee64378a4e59c547a3cd42e32be274a63dd1
Jazz Cheema <jaskamalc@gmail.com>
2024-04-02 12:29:47 -0700

    finished further study

--- src/Eightball.jsx
+++ src/Eightball.jsx
@@ -16,21 +16,42 @@ import { answersList } from './AnswersList.jsx';
  */
 function EightBall({ answers = answersList }) {
     const [answer, setAnswer] = useState({ msg: 'Think of a Question', color: 'black' });
-
+    const [ballColor, setBallColor] = useState({green: 0, red: 0, goldenrod:0})
 
     function handleClick(evt) {
         let randomAnswer = sample(answers);
         setAnswer(randomAnswer);
+        if(randomAnswer.color === 'green'){
+            ballColor.green += 1
+            setBallColor(ballColor)
+        }
+        if(randomAnswer.color === 'red'){
+            ballColor.red += 1
+            setBallColor(ballColor)
+        }
+        if(randomAnswer.color === 'goldenrod'){
+            ballColor.goldenrod += 1
+            setBallColor(ballColor)
+        }
+    }
+
+    function resetBall(evt){
+        setAnswer({msg:'Think of a Question', color: 'black'})
     }
 
 
     return (
-        <div>
+        <div className='Eightball-div'>
             <button className='Eightball' onClick={handleClick}
 
                 style={{ backgroundColor: answer.color }}>
                 {answer.msg}
             </button>
+            <br/>
+        <br/>
+        <br/>
+            <p>Colors: Green {ballColor.green} Goldenrod {ballColor.goldenrod} Red {ballColor.red}</p>
+            <button className='Eightball-reset' onClick={resetBall}>Reset</button>
         </div>
     );
 
